813cfbf6b50b6f1b5e82b6a0658add5b7b7db331
PhantomHerald <ankomahgerald@gmail.com>
2025-05-30 17:42:20 +0000

    Revamps onboarding and login flows with UI/UX improvements
    Replaces the tab index redirect with an onboarding screen to enhance the user introduction experience. Simplifies the login flow by removing Formik validation and improving state handling for better performance and maintainability.
    
    Introduces a new onboarding modal with animations, step-based navigation, and actionable buttons to improve user engagement. Updates styling across components to align with a modern dark theme.
    
    Upgrades several dependencies for compatibility and stability.

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,4 @@
-/*import { useNavigation } from "@react-navigation/native";
-import { Formik } from "formik";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
 import {
   Image,
@@ -9,80 +8,43 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import logo from "@/assets/Twitch1.png";
-import { useAuth } from "../auth/authContext";
 
 export default function Login() {
-  const { token, user, saveToken, saveUser } = useAuth();
-  const navigation = useNavigation();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = () => {
+    // You can add navigation or a simple alert here
+    alert("Logged in!");
+  };
 
   return (
     <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
+      <Image source={require("@/assets/Twitch1.png")} style={styles.logo} />
       <Text style={styles.title}>Login</Text>
-      <Formik
-        validationSchema={loginValidationSchema}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={submit}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-          isValid,
-        }) => (
-          <>
-            <View style={styles.inputContainer}>
-              <Icon name="mail-outline" size={25} style={styles.icon} />
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                keyboardType="email-address"
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-              />
-            </View>
-            {errors.email && touched.email && (
-              <Text style={styles.errorText}>{errors.email}</Text>
-            )}
-            <View style={styles.inputContainer}>
-              <Icon name="lock-closed-outline" size={25} style={styles.icon} />
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-                value={values.password}
-              />
-            </View>
-            {errors.password && touched.password && (
-              <Text style={styles.errorText}>{errors.password}</Text>
-            )}
-            <TouchableOpacity onPress={() => navigation.navigate("Forget")}>
-              <Text style={styles.forgotPassword}>Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={handleSubmit}
-              disabled={!isValid}
-            >
-              <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
-              <Text style={styles.signUp}>
-                Don't have an account?{" "}
-                <Text style={styles.signUpLink}>Sign Up</Text>
-              </Text>
-            </TouchableOpacity>
-          </>
-        )}
-      </Formik>
+      <View style={styles.inputContainer}>
+        <Icon name="email-outline" size={25} style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="lock-outline" size={25} style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -92,7 +54,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     paddingHorizontal: 20,
   },
   logo: {
@@ -100,6 +62,7 @@ const styles = StyleSheet.create({
     width: 200,
     resizeMode: "contain",
     marginBottom: 20,
+    borderRadius: 100,
   },
   title: {
     fontSize: 32,
@@ -112,7 +75,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height: 50,
-    backgroundColor: "#f1f1f1",
+    backgroundColor: "#28282E",
     borderRadius: 8,
     paddingHorizontal: 10,
     marginBottom: 20,
@@ -123,16 +86,12 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     height: "100%",
-  },
-  forgotPassword: {
-    alignSelf: "flex-end",
-    marginBottom: 20,
-    color: "#000",
+    color: "#fff",
   },
   button: {
     width: "100%",
     height: 50,
-    backgroundColor: "#1E90FF",
+    backgroundColor: "#9147FF",
     borderRadius: 8,
     justifyContent: "center",
     alignItems: "center",
@@ -142,21 +101,4 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 18,
   },
-  signUp: {
-    color: "#000",
-  },
-  signUpLink: {
-    color: "#1E90FF",
-  },
-  errorText: {
-    color: "red",
-    alignSelf: "flex-start",
-    marginBottom: 10,
-  },
 });
-
-// remember to install these dependencies
-/*
-install freepik and somm others and impelement the springboot backend
-npm install formik yup @react-navigation/native @react-navigation/stack react-native-vector-icons
-*/
---
+++ app/onboardingscreen.tsx
@@ -0,0 +1,292 @@
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { useRouter } from "expo-router";
+
+import { useEffect, useState } from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  Modal,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Animated, {
+  FadeInLeft,
+  FadeInRight,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+type Step = {
+  image: ImageSourcePropType;
+  title: string;
+  description: string;
+};
+
+const steps: Step[] = [
+  {
+    image: require("@/assets/images/favicon.png"),
+    title: "Welcome to Twitch",
+    description:
+      "Discover live streams, chat with your favorite creators, and join a vibrant community. Let’s get started!",
+  },
+  {
+    image: require("@/assets/images/favicon.png"),
+    title: "Follow & Chat",
+    description:
+      "Follow channels you love and never miss a stream. Join the chat to interact and be part of the action in real time.",
+  },
+  {
+    image: require("@/assets/images/favicon.png"),
+    title: "Go Live or Watch",
+    description:
+      "Stream your own content or explore thousands of live channels. Twitch is where your interests come alive!",
+  },
+];
+
+const OnboardingModal: React.FC = () => {
+  const [visible, setVisible] = useState(true);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [direction, setDirection] = useState<"forward" | "backward">("forward");
+  const router = useRouter();
+
+  const progressPosition = useSharedValue(0);
+
+  useEffect(() => {
+    progressPosition.value = currentStep / (steps.length - 1);
+  }, [currentStep]);
+
+  const progressAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      left: withSpring(progressPosition.value * 60 + 2.5, {
+        damping: 15,
+        stiffness: 100,
+      }),
+    };
+  });
+
+  const hideModal = () => setVisible(false);
+
+  const goToNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      setDirection("forward");
+      setCurrentStep(currentStep + 1);
+    } else {
+      hideModal();
+    }
+  };
+
+  const goToPreviousStep = () => {
+    if (currentStep > 0) {
+      setDirection("backward");
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const skipToLastStep = () => {
+    setDirection("forward");
+    setCurrentStep(steps.length - 1);
+  };
+
+  const getEnteringAnimation = () => {
+    return direction === "forward"
+      ? FadeInRight.duration(350)
+      : FadeInLeft.duration(350);
+  };
+
+  const goToSignIn = () => {
+    setVisible(false);
+    router.replace("/Home");
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      onRequestClose={hideModal}
+      animationType="slide"
+      presentationStyle="fullScreen"
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Animated.View
+            style={styles.imageWrapper}
+            entering={getEnteringAnimation()}
+            key={`image-${currentStep}`}
+          >
+            <Image source={steps[currentStep].image} style={styles.image} />
+          </Animated.View>
+        </View>
+        <View style={styles.contentContainer}>
+          <Animated.View
+            entering={getEnteringAnimation()}
+            key={`text-${currentStep}`}
+          >
+            <Text style={styles.title}>{steps[currentStep].title}</Text>
+            <Text style={styles.description}>
+              {steps[currentStep].description}
+            </Text>
+          </Animated.View>
+          <View style={styles.progressBar}>
+            <Animated.View
+              style={[styles.progressIndicator, progressAnimatedStyle]}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                currentStep === 0 && styles.disabledButton,
+              ]}
+              onPress={goToPreviousStep}
+              disabled={currentStep === 0}
+            >
+              <Icon name="chevron-left" size={18} color="#fff" />
+              <Text style={styles.buttonText}>Back</Text>
+            </TouchableOpacity>
+            {currentStep === steps.length - 1 ? (
+              <View>
+                <TouchableOpacity
+                  style={[styles.button, styles.loginButton]}
+                  onPress={goToSignIn}
+                >
+                  <Text style={styles.buttonText}>LOGIN</Text>
+                  <Icon name="login" size={18} color="#fff" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.button, styles.signUpButton]}
+                  onPress={goToSignIn}
+                >
+                  <Text style={styles.buttonText}>SIGN UP</Text>
+                  <Icon name="account" size={18} color="#fff" />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <TouchableOpacity
+                style={[styles.button, styles.nextButton]}
+                onPress={goToNextStep}
+              >
+                <Text style={styles.buttonText}>NEXT</Text>
+                <Icon name="arrow-right" size={18} color="#fff" />
+              </TouchableOpacity>
+            )}
+            <TouchableOpacity
+              style={[
+                styles.button,
+                currentStep === steps.length - 1 && styles.disabledButton,
+              ]}
+              onPress={skipToLastStep}
+              disabled={currentStep === steps.length - 1}
+            >
+              <Text style={styles.buttonText}>Skip</Text>
+              <Icon name="skip-next" size={18} color="#fff" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0e0e10", // dark background
+  },
+  imageContainer: {
+    height: "55%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageWrapper: {
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  contentContainer: {
+    height: "45%",
+    backgroundColor: "#18181b", // slightly lighter dark
+    borderTopLeftRadius: 80,
+    borderTopRightRadius: 80,
+    alignItems: "center",
+  },
+  title: {
+    color: "#a259ff", // purple accent
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 24,
+    textAlign: "center",
+    lineHeight: 48,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 16,
+    marginTop: 16,
+    textAlign: "center",
+    paddingHorizontal: 24,
+  },
+  progressBar: {
+    backgroundColor: "#28282E",
+    height: 14,
+    width: 80,
+    marginTop: 20,
+    borderRadius: 7,
+    justifyContent: "center",
+  },
+  progressIndicator: {
+    height: 10,
+    width: 20,
+    borderRadius: 5,
+    backgroundColor: "#a259ff", // purple accent
+    position: "absolute",
+  },
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 4,
+    marginTop: 44,
+  },
+  button: {
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    marginHorizontal: 5,
+  },
+  nextButton: {
+    borderWidth: 1,
+    borderColor: "#a259ff",
+    borderRadius: 20,
+    backgroundColor: "#a259ff22",
+  },
+  disabledButton: {
+    opacity: 0.5,
+  },
+  loginButton: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "#a259ff",
+    backgroundColor: "#a259ff22",
+    marginBottom: 8,
+  },
+  signUpButton: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "#fff",
+    marginTop: 12,
+    backgroundColor: "#28282E",
+  },
+});
+
+export default OnboardingModal;
